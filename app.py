001d6fba07e132920684a5e5f9fdb66bafae1447
Ruth Dorton <dortonruth@gmail.com>
2021-04-08 20:30:46 -0400

    Flask route start.

--- app.py
+++ app.py
@@ -1,10 +1,39 @@
-# Import the Flask dependency
-from flask import Flask
+# Import Dependencies
+import datetime as dt
+import numpy as np
+import pandas as pd
 
-# Create a new Flask instance
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+# Set database engine
+engine = create_engine("sqlite:///hawaii.sqlite")
+Base = automap_base()
+
+Base.prepare(engine, reflect=True)
+
+# Set the variables for classes
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Create a session link
+session = Session(engine)
+
+# Define Flask app
 app = Flask(__name__)
 
-# Create the route
-@app.route('/')
-def hello_world():
-    return 'Hello world'
+# Create the Welcome route
+def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''')
